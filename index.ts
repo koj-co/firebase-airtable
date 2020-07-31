48ef8672e2b13be334d704918103492626eb18d4
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-31 16:34:38 +0530

    :sparkles: Add firebase connection

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import { cosmicSync, config } from "@anandchowdhary/cosmic";
+import { initializeApp, credential, firestore } from "firebase-admin";
+import axios from "axios";
+
 cosmicSync("firebase-airtable");
 
+initializeApp({
+  credential: credential.cert(JSON.parse(config("firebaseServiceAccountKey"))),
+  databaseURL: config("firebaseDatabaseUrl"),
+});
+
+const subscribers = firestore().collection("subscribers-v2");
+
+const sent: string[] = [];
 const firebaseAirtable = async () => {
   console.log("Starting...");
-  console.log(config("firebaseDatabaseUrl"));
+  const docs = await subscribers.get();
+  docs.forEach((doc) => sent.push(doc.id));
+  console.log(sent);
 };
 
 firebaseAirtable();
